7e754eafa85800768e46c22cc538e02979905c02
carlisliu <carlisliu@gmail.com>
2016-05-04 12:42:49 +0800

    change compose table configuration method

--- tools/table-name/index.js
+++ tools/table-name/index.js
@@ -1,65 +1,7 @@
 "use strict";
-
-//SELECT * FROM SysObjects
-
-var tableNames = ['advertising',
-    'base_admin_scenic_sopt_rela',
-    'base_admin_token',
-    'base_adminuserinfo',
-    'base_roleright',
-    'base_roles',
-    'base_sysloginlog',
-    'base_sysmenu',
-    'base_user_scenicspot',
-    'base_userright',
-    'base_userrole',
-    'city',
-    'city_extend',
-    'city_images',
-    'city_special_recommendation',
-    'county',
-    'county_images',
-    'dishes',
-    'dishes_images',
-    'entertainment',
-    'entertainment_images',
-    'hotel',
-    'hotel_service',
-    'hotel_service_images',
-    'journey_base_info',
-    'journey_info',
-    'landscape',
-    'landscape_images',
-    'landscape_type',
-    'region',
-    'restaurant',
-    'scenic_spot',
-    'scenic_spot_activity',
-    'scenic_spot_comments',
-    'scenic_spot_extend',
-    'scenic_spot_feedback',
-    'scenic_spot_hotel_rela',
-    'scenic_spot_images',
-    'scenic_spot_live',
-    'scenic_spot_points',
-    'scenic_spot_restaurant_rela',
-    'scenic_spot_sos',
-    'scenic_spot_ticket',
-    'scenic_spot_user_images',
-    'search_index',
-    'special_recommendation',
-    'swim_together',
-    'swim_together_route',
-    'traffic_extend',
-    'travel_strategy_base',
-    'travel_strategy_trip',
-    'user_photo_album',
-    'user_photo_album_images',
-    'users',
-    'users_info',
-    'users_scenic_spot_trajectory',
-    'users_token'
-];
+var fs = require('fs');
+var path = require('path');
+var tableNames = require('./tables');
 
 function fcamelCase(all, letter) {
     return letter.toUpperCase();
@@ -71,20 +13,29 @@ function table2Entity(tables) {
         var entity = table.replace(/_([\da-z])/gi, fcamelCase);
         result.push(entity.charAt(0).toUpperCase() + entity.substr(1));
     });
-    result result;
+    return result;
 }
 
-var template = `
-<table tableName="advertising" domainObjectName="${table}"
-               enableCountByExample="false" enableUpdateByExample="false"
-               enableDeleteByExample="false" enableSelectByExample="false"
-               selectByExampleQueryId="false">
-        </table>
+function compose() {
+    var tables = table2Entity(tableNames);
+    var content = '';
+    tables.forEach(function  (table) {
+        var template = `<table tableName="advertising" domainObjectName="${table}"
+       enableCountByExample="false" enableUpdateByExample="false"
+       enableDeleteByExample="false" enableSelectByExample="false"
+       selectByExampleQueryId="false">
+</table>
 `;
-
-function config() {
-
+        content += template;
+    })
+    return content;
 }
 
-var content = "";
-
+var xmlContent = compose();
+if (xmlContent) {
+    fs.writeFile(path.resolve(__dirname, 'table-xml.txt'), xmlContent, function  (error) {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
