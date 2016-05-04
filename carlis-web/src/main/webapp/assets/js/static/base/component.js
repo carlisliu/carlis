274defdd319810541dc79266963f40f4e8815ca9
carlisliu <carlisliu@gmail.com>
2016-05-04 23:18:50 +0800

    change component

--- carlis-web/src/main/webapp/assets/js/static/base/component.js
+++ carlis-web/src/main/webapp/assets/js/static/base/component.js
@@ -1,5 +1,7 @@
 /**
  * Carlis Liu
+ * 
+ * Component Module
  */
 define('static/base/component', [ 'jquery', '../utils/util', '../utils/events' ], function(require, exports, module) {
 	var $ = require('jquery');
@@ -8,7 +10,7 @@ define('static/base/component', [ 'jquery', '../utils/util', '../utils/events' ]
 
 	function Component(container) {
 		this.container = $(container) || $('form');
-		//optimization for v8 hidden class
+		// optimization for v8 hidden class
 		this.validated = false;
 		Event.call(this);
 	}
@@ -27,17 +29,18 @@ define('static/base/component', [ 'jquery', '../utils/util', '../utils/events' ]
 
 	fn.save = function() {
 		var that = this;
-		
+
 		var data = this.data();
-		
+
 		this.emit('data', data, this);
-		
-		// add intercept for state check, if `check` event can proceed to this point, means all user validation already passed.
-		this.on('check', function(){
+
+		// add intercept for state check, if `check` event can proceed to this
+		// point, means all user validation already passed.
+		this.on('check', function() {
 			that.validated = true;
 		});
 		this.emit('check', this);
-		
+
 		if (this.validated) {
 			this.emit('submit', data, this);
 		}
